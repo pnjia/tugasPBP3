3759871440aa9469c8b6c1f3c8fb25f8b16235dd
Panji Angkasa Putra <133610889+pnjia@users.noreply.github.com>
2024-10-04 17:55:31 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,58 @@
+// percabangan
+
+function menghitungTotalKalori(menitLari=0, menitPushUp=0, menitPlank=0) {
+    let totalKalori = 0;
+
+    if (menitLari > 0) {
+        const kaloriPer5Menit = 60;
+        totalKalori += (menitLari / 5) * kaloriPer5Menit;
+    }
+
+    if (menitPushUp > 0) {
+        const kaloriPer30Menit = 200;
+        totalKalori += (menitPushUp / 30) * kaloriPer30Menit;
+    }
+
+    if (menitPlank > 0) {
+        const kaloriPer1Menit = 5;
+        totalKalori += menitPlank * kaloriPer1Menit;
+    }
+
+    return totalKalori;
+}
+
+console.log(`Total kalori: ${menghitungTotalKalori(5, 30)}`)
+
+
+
+
+
+// palindrome
+const kata = "kAsur ini rusak"
+let isPalindrome = false;
+let kataDibalik = '';
+for (let i = kata.length-1; i >= 0; i--) {
+  kataDibalik += `${kata[i]}`
+}
+if (kata.toLowerCase() === kataDibalik.toLowerCase()) {
+  isPalindrome = true
+}
+console.log(`kata '${kata}' merupakan palindrome: ${isPalindrome} `)
+
+
+
+
+function apakahPalindrome(num) {
+  const str = num.toString()  
+  return str === str.split('').reverse().join('')
+} 
+
+function mencariNextPalindrome(num) {
+  num++;
+  while(!apakahPalindrome(num)){
+    num++
+  }
+  return num
+}
+num = 90
+console.log(`Angka terdekat dari ${num} adalah ${mencariNextPalindrome(num)}`)
